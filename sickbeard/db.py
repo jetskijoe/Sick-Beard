38a634d7ceea158ae85e0d0883e424c2ba5cf0b1
Joe <jetskijoe@gmail.com>
2013-04-06 07:21:19 -0400

    db updates

--- sickbeard/db.py
+++ sickbeard/db.py
@@ -70,6 +70,44 @@ class DBConnection:
             return int(result[0]["db_version"])
         else:
             return 0
+    def mass_action(self, querylist, logTransaction=False):
+        with db_lock:
+            if querylist == None:
+                return
+            sqlResult = []
+            attempt = 0
+            while attempt < 5:
+                try:
+                    for qu in querylist:
+                        if len(qu) == 1:
+                            if logTransaction:
+                                logger.log(qu[0], logger.DEBUG)
+                            sqlResult.append(self.connection.execute(qu[0]))
+                        elif len(qu) > 1:
+                            if logTransaction:
+                                logger.log(qu[0] + " with args " + str(qu[1]), logger.DEBUG)
+                            sqlResult.append(self.connection.execute(qu[0], qu[1]))
+                    self.connection.commit()
+                    logger.log(u"Transaction with " + str(len(querylist)) + u" query's executed", logger.DEBUG)
+                    return sqlResult
+                except sqlite3.OperationalError, e:
+                    sqlResult = []
+                    if self.connection:
+                        self.connection.rollback()
+                    if "unable to open database file" in e.message or "database is locked" in e.message:
+                        logger.log(u"DB error: " + ex(e), logger.WARNING)
+                        attempt += 1
+                        time.sleep(1)
+                    else:
+                        logger.log(u"DB error: " + ex(e), logger.ERROR)
+                        raise
+                except sqlite3.DatabaseError, e:
+                    sqlResult = []
+                    if self.connection:
+                        self.connection.rollback()
+                    logger.log(u"Fatal error executing query: " + ex(e), logger.ERROR)
+                    raise
+            return sqlResult
     def action(self, query, args=None):
 
         with db_lock:
