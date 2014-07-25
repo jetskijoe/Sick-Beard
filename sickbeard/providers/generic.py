267a632018f4a2bfbd19ccc1d8e3de83fbab0577
Joe <jetskijoe@gmail.com>
2014-07-25 09:22:23 -0400

    test

--- sickbeard/providers/generic.py
+++ sickbeard/providers/generic.py
@@ -23,25 +23,26 @@ import datetime
 import os
 import re
 import itertools
+import Queue
 import sickbeard
+import requests
 
-from lib import requests
-from lib.feedparser import feedparser
 from sickbeard import helpers, classes, logger, db
-from sickbeard.common import MULTI_EP_RESULT, SEASON_RESULT, cpu_presets
+from sickbeard.common import MULTI_EP_RESULT, SEASON_RESULT
 from sickbeard import tvcache
 from sickbeard import encodingKludge as ek
 from sickbeard.exceptions import ex
-from lib.hachoir_parser import createParser
 from sickbeard.name_parser.parser import NameParser, InvalidNameException, InvalidShowException
 from sickbeard.common import Quality
 
+from lib.hachoir_parser import createParser
 
 class GenericProvider:
     NZB = "nzb"
     TORRENT = "torrent"
 
     def __init__(self, name):
+        self.queue = Queue.Queue()
 
         # these need to be set in the subclass
         self.providerType = None
@@ -287,7 +288,7 @@ class GenericProvider:
 
             # parse the file name
             try:
-                myParser = NameParser(False, showObj=show, convert=True)
+                myParser = NameParser(False, convert=True)
                 parse_result = myParser.parse(title)
             except InvalidNameException:
                 logger.log(u"Unable to parse the filename " + title + " into a valid episode", logger.DEBUG)
